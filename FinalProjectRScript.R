547012e91936ba243d41b17b283ac903b6649425
cchami2 <chante.hamilton@emory.edu>
2023-08-25 12:30:03 -0400

    Added labels to variables

--- FinalProjectRScript.R
+++ FinalProjectRScript.R
@@ -6,4 +6,9 @@ tuition_income_data <- read_csv(here::here("data","tuition_income.csv"))
 tbl_summary(
 	tuition_income_data,
 	by = income_lvl,
-	include = c(state, campus))
+	include = c(state, campus),
+label = list(
+	state ~ "State Name",
+	campus ~ "On or off-campus"
+),
+missing_text = "Missing")
