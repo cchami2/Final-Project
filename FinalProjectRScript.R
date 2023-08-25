d4d4b47285017f70c22967cd8a8bcf58f5e6fffa
cchami2 <chante.hamilton@emory.edu>
2023-08-25 16:45:34 -0400

    performed multivariate regression created a histogram

--- FinalProjectRScript.R
+++ FinalProjectRScript.R
@@ -2,12 +2,12 @@ library(tidyverse)
 library(gtsummary)
 library(here)
 library(dplyr)
+library(ggplot2)
 tuition_income_data <- read_csv(here::here("data","tuition_income.csv"))
 col_types = cols(campus = col_factor(levels = c("On Campus",
 																								"Off Campus")), income_lvl = col_factor(levels = c("0 to 30000",
-																																																	 "30001 to 48000", "48001 to 75000",
-																																																	 "75001 to 110000", "Over 110000")))
-tbl_summary(
+																																																	 "30001 to 48000", "48001 to 75000",																																																	 "75001 to 110000", "Over 110000")))
+table1 <- tbl_summary(
 	tuition_income_data,
 	by = campus,
 	include = c(state, income_lvl, net_cost),
@@ -23,3 +23,20 @@ missing_text = "Missing") |>
 	bold_labels() |>
 	modify_footnote(update = everything() ~ NA) |>
 	modify_header(label = "**Variable**", p.value = "**P**")
+
+#Multivariate Linear Regression
+
+	linear_model <- lm(net_cost ~ income_lvl + campus + state,
+										 data = tuition_income_data)
+	 table2 <- tbl_regression(
+		linear_model,
+		intercept = TRUE,
+		label = list(
+			income_lvl ~ "Income level",
+			campus ~ "On or off-campus",
+			state ~ "State name"
+		))
+#Creating a figure
+	 barplot <- ggplot(data.frame(tuition_income_data$income_lvl), aes(x=tuition_income_data$income_lvl)) +
+	 	geom_bar()
+
