33a1f3b459a9704816f2800f38f0aa5f692f5fea
cchami2 <chante.hamilton@emory.edu>
2023-08-25 17:17:15 -0400

    created a function to calcuate average

--- FinalProjectRScript.R
+++ FinalProjectRScript.R
@@ -11,12 +11,12 @@ table1 <- tbl_summary(
 	tuition_income_data,
 	by = campus,
 	include = c(state, income_lvl, net_cost),
-label = list(
-	state ~ "State name",
-	income_lvl ~ "Income level",
-	net_cost ~ "Average net cost"
-),
-missing_text = "Missing") |>
+	label = list(
+		state ~ "State name",
+		income_lvl ~ "Income level",
+		net_cost ~ "Average net cost"
+	),
+	missing_text = "Missing") |>
 	add_p(test = list(all_continuous() ~ "t.test",
 										all_categorical() ~ "chisq.test")) |>
 	add_overall(col_label = "**Total**") |>
@@ -24,19 +24,28 @@ missing_text = "Missing") |>
 	modify_footnote(update = everything() ~ NA) |>
 	modify_header(label = "**Variable**", p.value = "**P**")
 
-#Multivariate Linear Regression
+#Multivariate Regression Analysis
 
-	linear_model <- lm(net_cost ~ income_lvl + campus + state,
-										 data = tuition_income_data)
-	 table2 <- tbl_regression(
-		linear_model,
-		intercept = TRUE,
-		label = list(
-			income_lvl ~ "Income level",
-			campus ~ "On or off-campus",
-			state ~ "State name"
-		))
+linear_model <- lm(net_cost ~ income_lvl + campus + state,
+									 data = tuition_income_data)
+table2 <- tbl_regression(
+	linear_model,
+	intercept = TRUE,
+	label = list(
+		income_lvl ~ "Income level",
+		campus ~ "On or off-campus",
+		state ~ "State name"
+	))
 #Creating a figure
-	 barplot <- ggplot(data.frame(tuition_income_data$income_lvl), aes(x=tuition_income_data$income_lvl)) +
-	 	geom_bar()
+barplot <- ggplot(data.frame(tuition_income_data$income_lvl), aes(x=tuition_income_data$income_lvl)) +
+	geom_bar()
+
+#Created a function to calculate average
+myaverage <- function(net_cost) {
+	n <- length(net_cost)
+	averageval <- sum(net_cost) / n
+	return(averageval)
+}
+myaverage(tuition_income_data$net_cost)
+myaverage(tuition_income_data$total_price)
 
